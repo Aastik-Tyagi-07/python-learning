8181933a71d0948af5e6333abdf89fc3947b6fc9
Aastik Tyagi <aastik2126@gmail.com>
2026-08-11 22:05:14 +0530

    Completed Practice Set 2 questions 4 and 5

---
+++ Practice Set 2/4(2).py
@@ -0,0 +1,8 @@
+password = "aastik"
+entered_password =input("Enter password:")
+
+while (entered_password != password):
+    entered_password =input("Wrong password!! Enter password again:")
+
+print("You are successfully logged in.")
+    
